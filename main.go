9c9b39d7436e8deb1903b55346b2930a35ef7207
click2cloud-lamda <click2cloud-lamda@click2cloud.net>
2022-03-21 11:31:06 +0530

    Hello test123
    Signed-off-by: click2cloud-lamda <click2cloud-lamda@click2cloud.net>

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"test007/test"
+)
+
+func main() {
+	fmt.Println("Git checking proces11215")
+	test.File()
+}
---
+++ test/file.go
@@ -0,0 +1,7 @@
+package test
+
+import "fmt"
+
+func File() {
+	fmt.Println("Git checking file154463135134")
+}
